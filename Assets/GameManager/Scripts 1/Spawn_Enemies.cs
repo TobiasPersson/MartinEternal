8765734b390416aabb9610e100cd8ad047de9bd4
JustaConfusedGuy <56404874+JustaConfusedGuy@users.noreply.github.com>
2021-05-26 10:18:00 +0200

    Simple instantiate for enemies

---
+++ Assets/GameManager/Scripts 1/Spawn_Enemies.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Spawn_Enemies : MonoBehaviour
+{
+    public List<GameObject> enemyList_spawner = new List<GameObject>();
+    public List<GameObject> spawnPointsList_spawner = new List<GameObject>();
+    [SerializeField]
+    private float time = 0.0f;
+    [SerializeField]
+    private float timeLimit;
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time >= timeLimit){
+            generateRandomEnemy();
+            time = 0;
+        }
+    }
+
+    public void generateRandomEnemy(){
+        for(int i = 0; i < 3; i++){  
+        int enemy_random = Random.Range(0, enemyList_spawner.Count);
+        int spawn_random = Random.Range(0, spawnPointsList_spawner.Count);
+        
+        Instantiate(enemyList_spawner[enemy_random], spawnPointsList_spawner[spawn_random].transform.position, Quaternion.identity);
+        }
+    }
+}
