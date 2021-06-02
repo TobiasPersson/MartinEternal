786e4975f9d1ca264e5d614d8ad2fcce19911fbf
M1crob1ology <luca.belluso@elev.ga.lbs.se>
2021-06-02 09:15:04 +0200

    Pause Menu

---
+++ Assets/Pause.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    bool GamePaused = false;
+
+    public GameObject PauseCanvas;
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        PauseCanvas.SetActive(true);
+        GamePaused = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        PauseCanvas.SetActive(false);
+        GamePaused = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (!GamePaused && Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+
+        else if (GamePaused && Input.GetKeyDown(KeyCode.Escape))
+        {
+            ResumeGame();
+        }
+    }
+
+}
